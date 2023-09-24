6d8cb1007041d6e171ec06c68d553b3a5717b1df
btsmith1212 <135077506+btsmith1212@users.noreply.github.com>
2023-09-24 18:23:46 -0500

    added comment routes and models, first draft of retooling from framework

--- models/index.js
+++ models/index.js
@@ -1,13 +1,22 @@
-const User = require("./User");
-const Project = require("./Project");
+const User = require('./User');
+const Blog = require('./Blog');
+const Comment = require('./Comment');
 
-User.hasMany(Project, {
-  foreignKey: "user_id",
-  onDelete: "CASCADE",
+User.hasMany(Blog, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE',
 });
 
-Project.belongsTo(User, {
-  foreignKey: "user_id",
+Blog.belongsTo(User, {
+  foreignKey: 'user_id',
 });
 
-module.exports = { User, Project };
+Blog.hasMany(Comment, {
+  foreignKey: 'blog_id',
+});
+
+Comment.belongsTo(User, {
+  foreignKey: 'user_id',
+});
+
+module.exports = { User, Blog, Comment };
