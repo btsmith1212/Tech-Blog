c98c45ad8491cb28a5a223b074f1b0ad510325dd
Blake <btsmith1212@gmail.com>
2025-03-02 22:14:27 -0600

    edit

--- public/js/login.js
+++ public/js/login.js
@@ -14,7 +14,7 @@ const loginFormHandler = async (event) => {
     if (response.ok) {
       document.location.replace('/dashboard');
     } else {
-      alert(response.statusText);
+      alert('Incorrect Username and/or Password');
     }
   }
 };
